1a15f6622711c5ab2a617b6ac3b14cef63b2b582
Chris Birster <christopher.birster@gmail.com>
2022-10-11 19:03:15 -0400

    google slides embed

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,18 @@
+import invariant from "tiny-invariant";
+
+invariant(process.env.SLIDES_SRC, "SLIDES_SRC must be set");
+
+const slides = process.env.SLIDES_SRC;
+
 export default function Index() {
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-
+    <main className="min-h-screen min-w-screen bg-black sm:flex sm:items-center sm:justify-center">
+      <iframe
+        className="w-screen h-screen"
+        title="elks current events"
+        src={slides}
+        frameBorder="0"
+      />
     </main>
   );
 }
